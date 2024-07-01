2b62d6bf14d7386d7cba10b28764f6bd61e4c972
Franco Nardella <franconardella94@gmail.com>
2024-07-01 12:41:53 -0300

    creación Layout

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,5 @@
+import * as s from "./NavbarStyles";
+
+export const Navbar = () => {
+  return <s.HeaderContainer></s.HeaderContainer>;
+};
---
+++ src/components/Navbar/NavbarStyles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  height: 100px;
+  background-color: #18191a;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 4rem;
+`;
