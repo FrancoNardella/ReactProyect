222eebf783f1f54c25fddbf1b7d598d0118f34b1
Franco Nardella <franconardella94@gmail.com>
2024-07-02 11:16:11 -0300

    Add: Se modificó el  NavbarStyles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,18 @@
 import * as s from "./NavbarStyles";
+import imgLogo from "/public/img/huella.png";
 
 export const Navbar = () => {
   return (
     <s.HeaderContainer>
       <a href="#">
-        <img src="./huella.png" alt="logo" />
+        <img src={imgLogo} alt="logo" />
       </a>
-      <s.LinksContainer>
-        <s.NavbarContainer>
+      <s.NavbarContainer>
+        <s.NavbarItems>
           <a href="#">Nosotros</a>
           <a href="#">Productos</a>
-        </s.NavbarContainer>
-
-        {/* <s.UserNav>
-          <s.UserContainer>
-            <s.SpanStyled>Inicia sesion</s.SpanStyled>
-          </s.UserContainer>
-        </s.UserNav> */}
-      </s.LinksContainer>
+        </s.NavbarItems>
+      </s.NavbarContainer>
     </s.HeaderContainer>
   );
 };
--- src/components/Navbar/NavbarStyles.js
+++ src/components/Navbar/NavbarStyles.js
@@ -2,77 +2,41 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   height: 100px;
-  background-color: #18191a;
+  background-color: #f5e0b1;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 1rem 4rem;
+
+  img {
+    width: 70px;
+    height: 70px;
+  }
 `;
 
-export const LinksContainer = styled.div`
+export const NavbarContainer = styled.div`
   color: white;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 40px;
-
+  /* border: solid 2px red; */
   a {
     padding: 1rem 1.5rem;
   }
-  /* a:first-child {
-    background: var(--orange-bg);
-    border-radius: 1rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 5px;
-  } */
 `;
 
-export const LinkContainer = styled.div`
+export const NavbarItems = styled.div`
   font-size: 1.2rem;
-  color: ${(props) => (props.home ? "#ff9d01" : "#ff9300")};
-`;
-
-// export const UserContainer = styled(LinkContainer)`
-//   display: flex;
-//   align-items: center;
-// `;
-
-export const NavbarContainer = styled(LinkContainer)`
   display: flex;
   align-items: center;
+  /* border: solid 2px red; */
+
   @media (max-width: 768px) {
     display: none;
   }
-`;
 
-// export const UserNav = styled.div`
-//   gap: 15px;
-//   cursor: pointer;
-//   span {
-//     color: white;
-//     font-size: 1rem;
-//     margin-right: 20px;
-//   }
-
-//   @media (max-width: 768px) {
-//     display: none;
-//   }
-// `;
-
-// export const SpanStyled = styled.span`
-//   &:hover {
-//     text-decoration: underline;
-//   }
-// `;
-
-export const MenuContainer = styled(LinkContainer)`
-  font-size: 2rem;
-  display: none;
-
-  @media (max-width: 768px) {
-    display: flex;
-    align-items: center;
+  a {
+    color: #000000;
   }
 `;
